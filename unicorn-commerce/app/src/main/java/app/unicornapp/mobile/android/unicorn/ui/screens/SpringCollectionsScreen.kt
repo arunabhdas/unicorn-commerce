c91bfbb5929da948a4f607da8740141682b786f0
Arunabh Das <arunabhdas@gmail.com>
2023-03-10 08:18:25 -0500

    Added implementation of SpringCollectionsScreen

--- unicorn-commerce/app/src/main/java/app/unicornapp/mobile/android/unicorn/ui/screens/SpringCollectionsScreen.kt
+++ unicorn-commerce/app/src/main/java/app/unicornapp/mobile/android/unicorn/ui/screens/SpringCollectionsScreen.kt
@@ -3,17 +3,27 @@ package app.unicornapp.mobile.android.unicorn.ui.screens
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import app.unicornapp.mobile.android.unicorn.R
@@ -34,21 +44,43 @@ fun SpringCollectionsScreen(
             contentScale = ContentScale.FillBounds,
             modifier = Modifier.matchParentSize()
         )
-        Text(
-            modifier = Modifier.clickable {
-                navController.navigate(Screen.HomeScreen.route) {
-                    popUpTo(Screen.HomeScreen.route) {
-                        inclusive = true
-                    }
-                }
-            },
-            text = "SpringCollectionScreen",
-            color = Color.White,
-            fontSize = MaterialTheme.typography.titleSmall.fontSize,
-            fontWeight = FontWeight.Bold
-        )
+        LazyVerticalGrid(
+            modifier = Modifier.padding(
+                top = 90.dp,
+                bottom = 50.dp
+            ),
+            columns = GridCells.Adaptive(150.dp)
+        ) {
+            items(500) { number ->
+                MyGridCell(number)
+            }
+        }
     }
+}
 
+@Composable
+fun MyGridCell(number: Int) {
+    Card(
+        modifier = Modifier.padding(8.dp),
+        elevation = 20.dp
+    ) {
+        Box(
+            modifier = Modifier.aspectRatio(1f),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = "Item " + number,
+                fontSize = 20.sp
+            )
+            Image(
+                painter = painterResource(id = R.drawable.tshirt_2),
+                contentDescription = "",
+                Modifier
+                    .height(200.dp)
+                    .clip(RoundedCornerShape(20.dp)),
+            )
+        }
+    }
 }
 
 @Preview
